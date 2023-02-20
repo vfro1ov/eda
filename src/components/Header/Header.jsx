fb3c425109094ad553fa2abe1b77c80fd1544f1f
Vladimir F <vof9696@gmail.com>
2023-02-20 03:24:54 +0300

    add Header element + css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,37 @@
 import './Header.scss';
+const menu = [
+	{ id: 1, name: 'Пицца' },
+	{ id: 2, name: 'Комбо' },
+	{ id: 3, name: 'Закуски' },
+	{ id: 4, name: 'Напитки' },
+	{ id: 5, name: 'Акции' },
+	{ id: 6, name: 'Контакты' },
+	{ id: 7, name: 'О нас' },
+];
 const Header = () => {
 	return (
 		<div className="header">
-			<h2>Eda dostavka</h2>
+			<div className="header-wrap">
+				<div className="header-logo">
+					<a href="/" className="header-logo" alt="logo">
+						<h2>Eda dostavka</h2>
+					</a>
+				</div>
+
+				<nav>
+					<ul className="menu">
+						{menu.map((menu, index) => {
+							return (
+								<li className="menu-element" key={index}>
+									<a href="#" alt="menu-elements">
+										{menu.name}
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</nav>
+			</div>
 		</div>
 	);
 };
